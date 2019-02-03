b99634022d1834f5a33e69738baba1f2af6073cf
Yonnick Brown <25hour24@gmail.com>
2019-02-03 20:41:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,13 +48,13 @@ def add_supply_to_memorial_day(holiday_hash, supply)
     end
   end
 end
-
-def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-  holiday_supplies.map do |seasons, holidays|
-  if seasons == :fall
-      binding.pry
-      end
-    end
-end
+#
+# def add_supply_to_memorial_day(holiday_hash, supply)
+#   # again, holiday_hash is the same as the ones above
+#   # add the second argument to the memorial day array
+#   holiday_supplies.map do |seasons, holidays|
+#   if seasons == :fall
+#       [seasons]
+#       end
+#     end
+# end
