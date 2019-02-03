810683eaa907153b38d78a6d33e2ac73c30f1124
Yonnick Brown <25hour24@gmail.com>
2019-02-03 20:44:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,3 +58,12 @@ end
 #       end
 #     end
 # end
+
+def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  holiday_supplies.map do |seasons, holidays|
+   if seasons == :fall
+       [seasons][holiday_name.to_sym]
+       end
+     end
+
+end
