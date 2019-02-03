812153ff95625431d083a0ac1240778a891b2ae9
Yonnick Brown <25hour24@gmail.com>
2019-02-03 20:52:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_supplies.map do |seasons, holidays|
    if seasons == :fall
-       run = [holiday_name] = supply_array
+       [holiday_name] = supply_array
        binding.pry
        end
      end
