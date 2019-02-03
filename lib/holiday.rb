8f7a639d51556f614bf1555a8fd49f73985d04cd
Yonnick Brown <25hour24@gmail.com>
2019-02-03 23:14:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,3 +89,16 @@ def all_supplies_in_holidays(holiday_hash)
       end
     end
 end
+
+def all_holidays_with_bbq(holiday_hash)
+  # return an array of holiday names (as symbols) where supply lists
+  # include the string "BBQ"
+  bbq_key = []
+  holiday_supplies.map do |seasons, holidays|
+      holidays.map do | holiday, item|
+        if item.include("BBQ")
+          bbq_key << holidau
+      end
+  end
+  bbq_key
+end
