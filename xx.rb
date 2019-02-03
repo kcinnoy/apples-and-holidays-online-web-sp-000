9f745d46693e07b86cf496190d9e69c65507f3b0
Yonnick Brown <25hour24@gmail.com>
2019-02-03 20:29:36 +0000

    Automatically backed up by Learn

--- xx.rb
+++ xx.rb
@@ -1,8 +1,6 @@
 require 'pry'
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
   holiday_supplies.map do |seasons, holidays|
   if seasons == :fall
       binding.pry
