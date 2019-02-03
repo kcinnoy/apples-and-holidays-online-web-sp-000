3e9ca1e7fe70671686dfc5a5a0d08c70b060ab36
Yonnick Brown <25hour24@gmail.com>
2019-02-03 23:14:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -98,6 +98,7 @@ def all_holidays_with_bbq(holiday_hash)
       holidays.map do | holiday, item|
         if item.include("BBQ")
           bbq_key << holidau
+        end
       end
   end
   bbq_key
